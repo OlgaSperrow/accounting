27e00334a9525db1d58512602ec102e88db6ece9
OlgaSperrow <osperling17@gmail.com>
2025-04-15 10:13:46 +0200

    PreloadState added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,13 +1,18 @@
 import {configureStore} from "@reduxjs/toolkit";
 import user from '../features/user/userSlice.ts'
 import token from '../features/token/tokenSlice.ts'
+import {UserProfile} from "../utils/types";
+
+const preloadedState =JSON.parse(localStorage.getItem('state') || '{}') as {user: UserProfile, token: string};
 
 export const store =configureStore({
     reducer: {
         user, token
-    }
+    },
+    preloadedState
 });
 
+store.subscribe(() => localStorage.setItem("state", JSON.stringify(store.getState())));
 
 export type RootState = ReturnType<typeof store.getState>
 export type AppDispatch = typeof store.dispatch
